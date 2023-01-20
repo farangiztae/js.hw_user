eebbabf23a28f3d3884b30d3879c62ebd55377b1
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2023-01-20 17:03:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let user1 = {
+      name:prompt("Name"),
+      age:prompt("Age")
+};
+console.log(user1);
+
+let user2 = {
+      name:prompt("Name"),
+      age:prompt("Age")
+};
+console.log(user2);
